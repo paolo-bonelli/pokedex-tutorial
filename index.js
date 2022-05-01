599e003d3bb01cd5ae797c7f566c347769c75545
Paolo Bonelli <bv.vincenzo@gmail.com>
2022-05-01 07:21:46 -0400

    createPokemonCard function now print pokemon IDs

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const poke_container = document.getElementById("poke-container");
 
 const pokemon_number = 151;
 
-const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    console.log(pokemon);
-    return 0;
-}
-
 const fetchPokemon = async () => {
     for (let i=0; i<pokemon_number; i++){
         await getPokemon(i+1);
     }
 }
 
-fetchPokemon()
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonCard(pokemon);
+    return 0;
+}
+
+function createPokemonCard(pokemon) {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    const pokeInnerHtml = `${pokemon.id}`;
+    pokemonEl.innerHTML = pokeInnerHtml;
+    poke_container.appendChild(pokemonEl)
+}
+
+window.addEventListener('load', fetchPokemon)
